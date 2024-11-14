e032a2073fdbfe5941fc55e78a257005cdd99b5b
Erzhan1337 <220103382@stu.sdu.edu.kz>
2024-11-14 14:28:11 +0500

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,9 +3,6 @@
 const api_key = "2a3c21f7203959050cb73bdefd2b2ae2";
 const imageBaseURL = "http://image.tmdb.org/t/p/";
 
-/*--------- 
-Fetch data from a server using the 'url' and passes the result in JSON data to a 'callback' function, along with an optional parameter if has 'optionalParam'.
-----------*/
 
 const fetchDataFromServer = function (url, callback, optionalParam) {
   fetch(url)
