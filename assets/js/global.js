c65783ed9ff6313e7d046ec9ee3e428b17a25c0d
Erzhan1337 <220103382@stu.sdu.edu.kz>
2024-11-14 14:28:54 +0500

    Update global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,13 +1,12 @@
 "use strict";
 
-/*----- Add event on multiple elements ------*/
 const addEventOnElements = function (elements, eventType, callback) {
   for (const elem of elements) {
     elem.addEventListener(eventType, callback);
   }
 };
 
-/*----- Toggle search box in mobile device ------*/
+
 const searchBox = document.querySelector("[search-box]");
 const searchTogglers = document.querySelectorAll("[search-toggler]");
 
@@ -15,9 +14,7 @@ addEventOnElements(searchTogglers, "click", function () {
   searchBox.classList.toggle("active");
 });
 
-/*
-Store MovieId in Local Storage when I click any movie card
-.*/
+
 const getMovieDetail = function (movieId) {
   window.localStorage.setItem("movieId", String(movieId));
 };
